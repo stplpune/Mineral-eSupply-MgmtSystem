a29ce4602e2d95bb732f1929c2fe7c20506dc1c2
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-09-19 17:17:11 +0530

    web layot page changes

--- src/app/web/web-layout/web-layout.component.ts
+++ src/app/web/web-layout/web-layout.component.ts
@@ -7,19 +7,19 @@ import { NavigationEnd, NavigationStart, Router } from '@angular/router';
   styleUrls: ['./web-layout.component.scss']
 })
 export class WebLayoutComponent implements OnInit {
-  hideHeader: boolean = true;
-  hideFooter: boolean = true;
+  hideHeader: boolean = false;
+  hideFooter: boolean = false;
 
   constructor(private router: Router) {
-    if (this.router.url == '/login' || this.router.url == '/forget-password') {
-      this.hideHeader = false;
-      this.hideFooter = false;
+    if (this.router.url != '/login' && this.router.url != '/forget-password') {
+      this.hideHeader = true;
+      this.hideFooter = true;
     }
   }
 
   ngOnInit(): void {
     this.router.events.subscribe((event: any) => {
-      if (event.url === '/login' || event.url === '/forget-password') {
+      if (event.url != '/login' || event.url != '/forget-password') {
         this.hideHeader = false;
         this.hideFooter = false;
       } else {
