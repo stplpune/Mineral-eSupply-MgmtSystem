bb7c70d18f4afb3e25e45ff6af041bf151a46126
stpl-somnath <somnath.khabale@shauryatechnosoft.com>
2022-09-19 11:19:41 +0530

    commit

--- src/app/web/login/login.component.ts
+++ src/app/web/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb:FormBuilder) { }
+  loginFrm !:FormGroup;
   ngOnInit(): void {
+    this.defaultForm();
+  }
+  defaultForm(){
+    this.loginFrm = this.fb.group({
+      userName :['',Validators.required],
+      passWord :['',Validators.required],
+      captcha:['']
+    })
+
   }
 
+
+  onSubmit(){
+
+  }
 }
