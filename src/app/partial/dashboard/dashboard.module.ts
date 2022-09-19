a149d20ecc035b7219c161895cc4360683408116
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-09-19 17:20:32 +0530

    dashboard_changes

--- src/app/partial/dashboard/dashboard.module.ts
+++ src/app/partial/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgApexchartsModule
   ]
 })
 export class DashboardModule { }
