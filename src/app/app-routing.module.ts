784f92f616cccb9d6180e2dc6594b18b8181a9c9
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-09-19 14:50:36 +0530

    take pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,22 +10,21 @@ const routes: Routes = [
     component: WebLayoutComponent,
     children: [
       { path: '', loadChildren: () => import('./web/web-layout/web-layout.module').then(m => m.WebLayoutModule) },
-        ]
+    ]
   },
   {
     path: '',
     component: PartialLayoutComponent,
     children: [
       { path: '', loadChildren: () => import('./partial/partial-layout/partial-layout.module').then(m => m.PartialLayoutModule), data: { title: 'Login' } },
-      { path: 'demo', loadChildren: () => import('./partial/demo/demo.module').then(m => m.DemoModule) },
     ]
-    
+
   },
- 
+
   { path: 'demo-bidder-reg', loadChildren: () => import('./web/demo-bidder-reg/demo-bidder-reg.module').then(m => m.DemoBidderRegModule) },
 
-  
-  
+
+
 ];
 
 @NgModule({
