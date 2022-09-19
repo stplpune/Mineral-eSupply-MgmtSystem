4c96e85329a9f7de7159bb69334e944037e3c630
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-09-19 14:55:14 +0530

    added_pattern_in_validation_file

--- src/app/core/services/forms-validation.service.ts
+++ src/app/core/services/forms-validation.service.ts
@@ -4,6 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FormsValidationService {
+  
+  excludeZeroAtStart = '^(?!0+$)[0-9]{0,20}$';
+  onlyAlphabets = '^[a-zA-Z]+$';
+  alphabetsWithSpaces = '^[a-zA-Z][a-zA-Z ]*$';
+  alphaNumeric = '^[a-zA-Z0-9]*$';
+  alphaNumericWithSpace = '^[a-zA-Z0-9][a-zA-Z0-9 ]*$';
+  alphaNumericWithSpaceAndSpecialChar = '^[a-zA-Z0-9 /(,)&.+-@#$]*$';
+  onlyDigits = '^[0-9]*$';
+  numbersWithDot = '^[0-9\s]*\.?[0-9\s]+$'
 
   constructor() { }
+
+  removeSpaceAtBegining(event: any) {
+    let temp = true;
+    try {
+        if (!event.target.value[0].trim()) {
+            event.target.value = event.target.value.substring(1).trim();
+            temp = false;
+        }
+    }
+    catch (e) {
+        temp = false;
+    }
+    return temp
+  }
+
 }
