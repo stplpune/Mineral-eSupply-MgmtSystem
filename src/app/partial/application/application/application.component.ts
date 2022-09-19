1a81bc572f8a464dab7c5d2b73ebb1ca8a45d278
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-09-19 19:04:08 +0530

    coal application page designed

--- src/app/partial/application/application/application.component.ts
+++ src/app/partial/application/application/application.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
   styleUrls: ['./application.component.scss']
 })
 export class ApplicationComponent implements OnInit {
-
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
