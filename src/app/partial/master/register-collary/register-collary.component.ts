3d716a2ffc8df0de04bfffa49f343daa318b68fc
Rahulmistari2141 <rahulmistari2141@gmail.com>
2022-09-20 14:08:27 +0530

    create new page

--- src/app/partial/master/register-collary/register-collary.component.ts
+++ src/app/partial/master/register-collary/register-collary.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './register-collary.component.html',
   styleUrls: ['./register-collary.component.scss']
 })
-export class RegisterCollaryComponent implements OnInit {
+export class RegisterCollaryComponent implements OnInit {displayedColumns: string[] = ['position','sr','dist', 'add','action',];
+dataSource = ELEMENT_DATA;
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+}
+
+
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, sr: '', dist:'', add:'', action: '', },
+ 
+];
+export interface PeriodicElement {
+position:number;
+sr: string;
+dist: string;
+add: string;
+action: string;
 
 }
--- src/app/partial/master/register-collary/register-collary.module.ts
+++ src/app/partial/master/register-collary/register-collary.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterCollaryRoutingModule } from './register-collary-routing.module';
 import { RegisterCollaryComponent } from './register-collary.component';
-
+import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { RegisterCollaryComponent } from './register-collary.component';
   ],
   imports: [
     CommonModule,
-    RegisterCollaryRoutingModule
+    RegisterCollaryRoutingModule,
+    AngularMaterialModule
   ]
 })
 export class RegisterCollaryModule { }
