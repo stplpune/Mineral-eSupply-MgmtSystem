89d1bc007a4addd1ff367049c942437a3890d49d
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-09-20 14:58:42 +0530

    add agm map comp

--- src/app/shared/shard.module.ts
+++ src/app/shared/shard.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgmMapComponent } from './components/agm-map/agm-map.component';
+import { AgmCoreModule } from '@agm/core';
+import { ConfigService } from '../configs/config.service';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AgmMapComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    AgmCoreModule.forRoot(ConfigService.googleApiObj),
+  ],
+  exports: [AgmMapComponent]
 })
 export class ShardModule { }
