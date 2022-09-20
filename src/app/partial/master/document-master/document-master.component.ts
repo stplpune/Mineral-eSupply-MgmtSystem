8b94a6bd7a3a791917dfbd394c53af27b2215377
stpl-somnath <somnath.khabale@shauryatechnosoft.com>
2022-09-20 15:39:52 +0530

    change in document and  coal reg

--- src/app/partial/master/document-master/document-master.component.ts
+++ src/app/partial/master/document-master/document-master.component.ts
@@ -1,11 +1,13 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormGroupDirective } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 import { CallApiService } from 'src/app/core/services/call-api.service';
 import { FormsValidationService } from 'src/app/core/services/forms-validation.service';
 import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Router } from '@angular/router';
+import { ConfigService } from 'src/app/configs/config.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-document-master',
@@ -13,14 +15,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./document-master.component.scss']
 })
 export class DocumentMasterComponent implements OnInit {
+  @ViewChild(FormGroupDirective) formGroupDirective!: FormGroupDirective;
   displayedColumns: string[] = ['position', 'sr', 'Mandatory', 'web',];
-  dataSource = ELEMENT_DATA;
+  dataSource: any;
   documentFrm !: FormGroup;
+
   constructor(private fb: FormBuilder,
     public commonMethod: CommonMethodsService,
     public apiService: CallApiService,
     public validation: FormsValidationService,
     public error: ErrorHandlerService,
+    public configService :ConfigService,
     private spinner: NgxSpinnerService, private router: Router) { }
   ngOnInit(): void {
     this.defaultForm();
@@ -38,6 +43,7 @@ export class DocumentMasterComponent implements OnInit {
     this.apiService.getHttp().subscribe({
       next: (res: any) => {
         if (res.statusCode === "200") {
+          this.dataSource = new MatTableDataSource(res.responseData.responseData1);
         } else {
           this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
         }
@@ -46,7 +52,7 @@ export class DocumentMasterComponent implements OnInit {
     })
   }
 
-  onSubmit() {
+  saveUpdate() {
     this.spinner.show();
     const formValue = this.documentFrm.value;
     if (this.documentFrm.invalid) {
@@ -67,6 +73,9 @@ export class DocumentMasterComponent implements OnInit {
     })
   }
 
+  clearAll(){
+    this.formGroupDirective.resetForm();
+  }
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
