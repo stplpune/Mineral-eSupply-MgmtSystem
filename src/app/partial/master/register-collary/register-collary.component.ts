37b8974abce2bcf066e0f22d0fc28b1aa1827915
sham843 <sham.jadhav@shauryatechnosoft.com>
2022-09-20 14:25:53 +0530

    add dynamic filter

--- src/app/partial/master/register-collary/register-collary.component.ts
+++ src/app/partial/master/register-collary/register-collary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/configs/config.service';
 
 @Component({
   selector: 'app-register-collary',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterCollaryComponent implements OnInit {displayedColumns: string[] = ['position','sr','dist', 'add','action',];
 dataSource = ELEMENT_DATA;
 
-constructor() { }
+constructor(public configService:ConfigService) { }
 
 ngOnInit(): void {
 }
