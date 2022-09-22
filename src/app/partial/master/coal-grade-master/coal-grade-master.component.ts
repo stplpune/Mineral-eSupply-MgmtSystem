c05e04a0df1562e1b44bb60d1ed1ba308c5ac02f
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-09-22 18:14:02 +0530

    coal grade page chnages

--- src/app/partial/master/coal-grade-master/coal-grade-master.component.ts
+++ src/app/partial/master/coal-grade-master/coal-grade-master.component.ts
@@ -9,71 +9,155 @@ import { CallApiService } from 'src/app/core/services/call-api.service';
 import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { WebStorageService } from 'src/app/core/services/web-storage.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { ConfirmationComponent } from '../../dialogs/confirmation/confirmation.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-coal-grade-master',
   templateUrl: './coal-grade-master.component.html',
   styleUrls: ['./coal-grade-master.component.scss']
 })
-export class CoalGradeMasterComponent implements OnInit {displayedColumns: string[] = ['position','sr', 'web',];
-dataSource: any;
-coalFrm !:FormGroup;
-@ViewChild(FormGroupDirective) formGroupDirective!: FormGroupDirective;
-constructor(private fb: FormBuilder,
-  public commonMethod: CommonMethodsService,
-  public apiService: CallApiService,
-  public validation: FormsValidationService,
-  public error: ErrorHandlerService,
-  public configService :ConfigService ,
-  private spinner: NgxSpinnerService, private router: Router) { }
+export class CoalGradeMasterComponent implements OnInit {
 
-ngOnInit(): void {
-  this.defaultForm();
-}
-defaultForm() {
-  this.coalFrm = this.fb.group({
-    coal: ['',[ Validators.required,Validators.pattern('^[^\\s\\[\\[`&._@#%*!+"\'\/\\]\\]{}][0-9a-zA-Z&s\\s]+$')]],
-  })
-}
+  displayedColumns: string[] = ['position', 'gradeName', 'action'];
+  dataSource: any;
+  coalFrm !: FormGroup;
+  @ViewChild(FormGroupDirective) formGroupDirective!: FormGroupDirective;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  pageNumber: number = 1;
+  pagesize: number = 10;
+  totalRows: any;
+  highlightedRow: any;
 
-getData() {
-  this.apiService.setHttp('get', "user-registration/", false, false, false, 'masterUrl');
-  this.apiService.getHttp().subscribe({
-    next: (res: any) => {
-      if (res.statusCode === "200") {
-        this.dataSource = new MatTableDataSource(res.responseData.responseData1);
-      } else {
-        this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
-      }
-    },
-    error: ((error: any) => { this.error.handelError(error.status) })
-  })
-}
 
-saveUpdate() {
-  this.spinner.show();
-  const formValue = this.coalFrm.value;
-  if (this.coalFrm.invalid) {
-    this.spinner.hide();
-    return;
+  constructor(private fb: FormBuilder,
+    public commonMethod: CommonMethodsService,
+    public apiService: CallApiService,
+    public validation: FormsValidationService,
+    public error: ErrorHandlerService,
+    public configService: ConfigService,
+    private webStorageService: WebStorageService,
+    public dialog: MatDialog,
+    private spinner: NgxSpinnerService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.defaultForm();
+    this.getCoalGrade();
   }
 
-  this.apiService.setHttp('get', "user-registration/" + formValue.userName.trim() + "/" + formValue.passWord.trim(), false, false, false, 'masterUrl');
-  this.apiService.getHttp().subscribe({
-    next: (res: any) => {
-      if (res.statusCode === "200") {
-        this.spinner.hide();
-      } else {
-        this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
-      }
-    },
-    error: ((error: any) => { this.error.handelError(error.status) })
-  })
-}
+  defaultForm() {
+    this.coalFrm = this.fb.group({
+      id: [0],
+      flag: [0],
+      coal: ['', [Validators.required, Validators.pattern('^[^\\s\\[\\[`&._@#%*!+"\'\/\\]\\]{}][0-9a-zA-Z&s\\s]+$')]],
+    })
+  }
 
-clearAll(){
-  this.formGroupDirective.resetForm();
-}
+  getCoalGrade() {
+    let obj = 'Search=' + '' + '&pageno=' + this.pageNumber + '&pagesize=' + this.pagesize;
+    this.apiService.setHttp('get', "CoalGrade?" + obj, false, false, false, 'WBMiningService');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == "200") {
+          this.dataSource = new MatTableDataSource(res.responseData?.responseData1);
+          this.totalRows = res.responseData.responseData2.totalCount;
+          this.totalRows > 10 && this.pageNumber == 1 ? this.paginator?.firstPage() : '';
+        } else {
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
+        }
+      },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    })
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.pageIndex + 1;
+    this.getCoalGrade();
+  }
+
+  saveUpdate() {
+    this.spinner.show();
+    if (this.coalFrm.invalid) {
+      this.spinner.hide();
+      return;
+    }
+
+    let obj = {
+      "id": this.coalFrm.value.id,
+      "gradeName": this.coalFrm.value.coal,
+      "createdby": this.webStorageService.getUserId(),
+      "flag": this.coalFrm.value.flag
+    }
+
+    this.apiService.setHttp('post', "CoalGrade", false, obj, false, 'WBMiningService');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == "200") {
+          this.formGroupDirective.resetForm();
+          this.defaultForm();
+          this.pageNumber = 1;
+          this.getCoalGrade();
+          this.spinner.hide();
+        } else {
+          this.spinner.hide();
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
+        }
+      },
+      error: ((error: any) => { this.error.handelError(error.status), this.spinner.hide(); })
+    })
+  }
+
+  updateCoalGrade(obj: any) {
+    this.coalFrm.patchValue({
+      id: obj.id,
+      coal: obj.gradeName,
+      flag: 1,
+    })
+  }
+
+  deleteConformation(id: any) {
+    this.highlightedRow = id;
+    let obj: any = ConfigService.dialogObj;
+    obj['p1'] = 'Are you sure you want to delete this record?';
+    obj['cardTitle'] = 'Delete';
+    obj['successBtnText'] = 'Delete';
+    obj['cancelBtnText'] = 'Cancel';
+    obj['inputType'] = false;
+    const dialog = this.dialog.open(ConfirmationComponent, {
+      width: this.configService.dialogBoxWidth[0],
+      data: obj,
+      disableClose: this.configService.disableCloseBtnFlag,
+    })
+    dialog.afterClosed().subscribe(res => {
+      if (res == 'Yes') {
+        this.deleteCoalGrade();
+      }
+    })
+  }
+
+  deleteCoalGrade() {  // this.highlightedRow == id
+    let obj = {
+      "id": this.highlightedRow,
+      "deletedBy": 1
+    }
+    
+    this.apiService.setHttp('DELETE', "CoalGrade/DeleteCoalGrade", false, obj, false, 'WBMiningService');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == "200") {
+          this.commonMethod.matSnackBar(res.statusMessage, 0);
+          this.pageNumber = 1;
+          this.getCoalGrade();
+        } else {
+          this.commonMethod.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);;
+        }
+      },
+      error: ((error: any) => { this.error.handelError(error.status) })
+    });
+  }
+
 
 }
 
