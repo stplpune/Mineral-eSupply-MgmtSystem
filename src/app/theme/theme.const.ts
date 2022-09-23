ba520137e33207ae5fac9bb7485130d370977852
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-09-23 13:45:13 +0530

    new theme colors added

--- src/app/theme/theme.const.ts
+++ src/app/theme/theme.const.ts
@@ -3,6 +3,7 @@ export const themes: any = {
         '--text-color': '#303030',
         '--bg-color': '#fff',
         '--top-bg-color': '#FBF3EC',
+        '--body-bg-color': '#E6EAEE',
         '--menu-bg-color': '#fff',
         '--card-bg-color': '#FFFFFF',
         '--card-border-color': 'rgba(0,0,0,0.125)'
@@ -11,7 +12,8 @@ export const themes: any = {
         '--text-color': '#fff',
         '--bg-color': '#2B3036',
         '--top-bg-color': '#141516',
-        '--menu-bg-color': '#0a0a0a',
+        '--body-bg-color': '#2B3036',
+        '--menu-bg-color': '#3F4853',
         '--card-bg-color': '#3F4853',
         '--card-border-color': '#3F4853'
     }
