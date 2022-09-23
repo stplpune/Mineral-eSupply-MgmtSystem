063befae1aaa2635086d3d8ebe3cea3d7b9b1926
stpl-somnath <somnath.khabale@shauryatechnosoft.com>
2022-09-23 21:08:53 +0530

    commit

--- src/app/core/services/common-api-call.service.ts
+++ src/app/core/services/common-api-call.service.ts
@@ -100,4 +100,24 @@ export class CommonApiCallService {
     })
   }
 
+  getMonthYear(){
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', "DropdownService/GetMonthYear", false, false, false, 'WBMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == 200) { obj.next(res.responseData); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      })
+    })
+  }
+
+  getCollieryNameList(){
+    return new Observable((obj) => {
+      this.apiService.setHttp('get', "api/BookingAndDeliveryOrder/getCollieryNameList", false, false, false, 'WBMiningService');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == 200) { obj.next(res.responseData); } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      })
+    })
+  }
+
 }
