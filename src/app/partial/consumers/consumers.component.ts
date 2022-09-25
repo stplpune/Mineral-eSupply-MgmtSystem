ca0923e08abcab26ec1142cda795d10ae374e510
Shital Kirolkar <shital.kirolkar@shauryatechnosoft.com>
2022-09-25 17:01:41 +0530

    consumers_changes

--- src/app/partial/consumers/consumers.component.ts
+++ src/app/partial/consumers/consumers.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfigService } from 'src/app/configs/config.service';
+import { CallApiService } from 'src/app/core/services/call-api.service';
+import { CommonApiCallService } from 'src/app/core/services/common-api-call.service';
+import { CommonMethodsService } from 'src/app/core/services/common-methods.service';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
 
 @Component({
   selector: 'app-consumers',
@@ -7,24 +16,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsumersComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  filterForm: FormGroup | any;
+  pageNumber: number = 1;
+  pagesize: number = 10;
+  totalRows: any;
+  dataSource: any;  
+  stateFilterArray: any[] = [];
+  displayedColumns: string[] = ['srno', 'consumerName', 'mobileNo', 'consumerTypeId', 'emailId', 'consumerDocuments', 'action'];
+  applicationTypeFilterArray = ['All', 'Individual', 'Organization'];
+
+  constructor(private fb: FormBuilder,
+    public callApiService: CallApiService,
+    public commonService: CommonMethodsService,
+    public errorSerivce: ErrorHandlerService,
+    public configService: ConfigService,
+    public commonApiCallService: CommonApiCallService,
+    private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.defaultFilterForm();
+    this.getConsumerRegistration();
+    this.getState();
   }
-  displayedColumns: string[] = ['srno', 'consumer_name', 'district', 'consumer_address','mobile_no','email','quantity_allocated','lifted_quantity','action'];
-  dataSource = ELEMENT_DATA;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, consumer_name: 'Hydrogen', district: 'maharashtra', consumer_address: 'H',mobile_no: 8669264767,email:'abc@gmail.com',quantity_allocated:12000,lifted_quantity:50000,action:''}
-];
-export interface PeriodicElement {
-  srno: number;
-  consumer_name: string;
-  district: string;
-  consumer_address: string;
-  mobile_no: number;
-  email: any;
-  quantity_allocated: number;
-  lifted_quantity: number;
-  action: any;
+
+  defaultFilterForm() {
+    this.filterForm = this.fb.group({
+      consumerType: [0],
+      stateId: [0],
+      searchText: [''],
+    })
+  }
+
+  getConsumerRegistration() {
+    let formData = this.filterForm.value;
+    let obj = 'Textsearch=' + formData.searchText?.trim() + '&ConsumerTypeId=' + parseInt(formData.consumerType) + '&StateId=' + formData.stateId + '&pageno=' + this.pageNumber + '&pagesize=' + this.pagesize
+    this.callApiService.setHttp('get', "api/ConsumerRegistration/GetConsumerDetails?" + obj, false, false, false, 'WBMiningService');
+    this.callApiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode == 200 && res.responseData.responseData1) {
+          this.dataSource = new MatTableDataSource(res.responseData.responseData1);
+          this.totalRows = res.responseData.responseData2.totalCount;
+          this.totalRows > 10 && this.pageNumber == 1 ? this.paginator?.firstPage() : '';
+          this.spinner.hide();
+        } else {
+          this.spinner.hide();
+          this.dataSource = [];
+          this.commonService.matSnackBar(res.statusMessage, 0);
+        }
+      },
+      error: ((error: any) => { this.errorSerivce.handelError(error.status) })
+    })
+  }
+
+  getState() {
+    this.commonApiCallService.getState().subscribe({
+      next: (response: any) => {
+        this.stateFilterArray.push({ text: "All", value: 0 }, ...response);
+      },
+      error: (err => { this.errorSerivce.handelError(err) })
+    })
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.pageIndex + 1;
+    this.getConsumerRegistration();
+  }
 }
