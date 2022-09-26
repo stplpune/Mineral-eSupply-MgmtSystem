5f75df726221af9d8bbad3f7c5a066116ced3ac5
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-09-26 11:38:02 +0530

    UI changes

--- src/app/partial/account-management/payment-receipt/payment-receipt.component.ts
+++ src/app/partial/account-management/payment-receipt/payment-receipt.component.ts
@@ -12,6 +12,8 @@ export interface PeriodicElement {
   remarks: string;
 }
 
+
+
 const ELEMENT_DATA: PeriodicElement[] = [
   {date: '12-02-2021', particulars: 'Purchase Order No. 215/25', debit: '53,100', credit:'-', remarks:'-'},
   {date: '14-02-2021', particulars: 'Purchase GST 5%', debit: '59,100', credit: '-', remarks:'-'},
@@ -30,6 +32,9 @@ export class PaymentReceiptComponent implements OnInit {
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'},
   ];
+  partyArray =['Julfikar Coke Briquette', 'Julfikar Coke Briquette','CHANDRA COKE BRIQUETTE','ASIA AND SONS','TAJ GUL FACTORY','STAR COKE BRIQUETTE' ,'Gobinda Coke Briquette','Bani Enterprise'];
+  paymentMode= ['Cash','Cheque','DD',"Net Banking",'UPI'];
+bankArray= ['ICICI Bank [909768768786]','HDFC [8457567676866]'];
   displayedColumns: string[] = ['date', 'particulars', 'debit', 'credit','remarks'];
   dataSource = ELEMENT_DATA;
   constructor() { }
